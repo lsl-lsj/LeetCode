f46c7a62788cb66ce57d652418958b746d44684d
laishilin <laishilin@ezgrid.cn>
2020-09-11 17:50:52 +0800

    一维数组的动态和

---
+++ Easy/2020-09-11/Demo03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+    给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+    请返回 nums 的动态和。
+*/
+namespace Demo03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            Solution s = new Solution();
+            var sums = s.RunningSum(nums);
+
+            Console.WriteLine(string.Join(",", sums));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int sum = 0;
+            List<int> sums = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                sums.Add(sum);
+            }
+
+            return sums.ToArray();
+        }
+    }
+}
