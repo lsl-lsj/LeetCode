160859e115b12ac2542a1a7b0a0f60a4f710923e
laishilin <laishilin@ezgrid.cn>
2020-09-15 10:38:52 +0800

    剑指 Offer 58 - II. 左旋转字符串

---
+++ Easy/2020-09-15/Demo01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+    字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+    比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。。
+*/
+namespace Demo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string str = s.ReverseLeftWords("abcdef1123  3213", 5);
+            Console.Write(str);
+        }
+    }
+
+    public class Solution
+    {
+        public string ReverseLeftWords(string s, int n)
+        {
+            return s.Remove(0, n) + s.Substring(0, n);
+        }
+    }
+}
