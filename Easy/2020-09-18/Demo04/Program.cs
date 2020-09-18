afa971dc8ea0db17c76cdbe51c264bd4ccda81da
laishilin <laishilin@ezgrid.cn>
2020-09-18 11:07:53 +0800

    好数对的数目

---
+++ Easy/2020-09-18/Demo04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/*
+    给你一个整数数组 nums 。
+    如果一组数字 (i,j) 满足 nums[i] == nums[j] 且 i < j ，就可以认为这是一组 好数对 。
+    返回好数对的数目。
+*/
+namespace Demo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 1 };
+            Solution s = new Solution();
+            Console.WriteLine(s.NumIdenticalPairs(nums));
+        }
+    }
+
+    public class Solution
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                for (int j = i + 1; j < nums.Length; ++j)
+                {
+                    if (nums[i] == nums[j])
+                        count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
