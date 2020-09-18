4b62189b8e0dab4756a03f10ebbb22cfc4d95e0f
laishilin <laishilin@ezgrid.cn>
2020-09-18 10:44:53 +0800

    整数的各位积和之差

---
+++ Easy/2020-09-18/Demo03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System;
+/*
+    给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+*/
+namespace Demo03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.SubtractProductAndSum(4421));
+        }
+    }
+
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int product = 1, sum = 0, length = n.ToString().Length;
+
+            for (int i = 1; i < length + 1; ++i)
+            {
+                int x = n % 10;
+
+                product *= x;
+                sum += x;
+
+                n = n / 10;
+            }
+
+            return product - sum;
+        }
+    }
+}
